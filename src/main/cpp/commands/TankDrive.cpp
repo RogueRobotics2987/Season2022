b85be947dbaff8e076a22c657dd52072d49cbbb5
=DylanSonnen <=dylansonnen@gmail.com>
2021-11-18 15:59:10 -0600

    initial commit

---
+++ src/main/cpp/commands/TankDrive.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/TankDrive.h"
+
+TankDrive::TankDrive(DriveTrain* drivetrain, frc::Joystick*stick1) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  m_drivetrain = drivetrain;
+  m_stick1 = stick1;
+  SetName("TankDrive");
+  AddRequirements({m_drivetrain});
+}
+
+// Called when the command is initially scheduled.
+void TankDrive::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void TankDrive::Execute() {
+
+  double outputY;
+  double outputX;
+
+  if (m_stick1->GetY() > 0.5) {
+    outputY = 0.5;
+  } else{
+    outputY = m_stick1->GetY();
+  }
+
+  if (m_stick1->GetX() > 0.5) {
+    outputX = 0.5;
+  } else{
+    outputX = m_stick1->GetX();
+  }
+
+  m_drivetrain->Drive(outputY, outputX);
+}
+
+// Called once the command ends or is interrupted.
+void TankDrive::End(bool interrupted) {m_drivetrain->Drive(0, 0);}
+
+// Returns true when the command should end.
+bool TankDrive::IsFinished() {
+  return false;
+}
---
+++ src/main/include/subsystems/DriveTrain.h
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include "rev/CANSparkMax.h"
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/Joystick.h>
+
+class DriveTrain : public frc2::SubsystemBase {
+ public:
+  DriveTrain();
+  void Drive(double y, double z);
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+ private:
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+  rev::CANSparkMax* LeftBack = new rev::CANSparkMax(56, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax* LeftFront = new rev::CANSparkMax(49, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANEncoder LeftEncoder = LeftFront->GetEncoder();
+  rev::CANSparkMax* RightBack = new rev::CANSparkMax(50, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANSparkMax* RightFront = new rev::CANSparkMax(46, rev::CANSparkMax::MotorType::kBrushless);
+  rev::CANEncoder RightEncoder = RightFront->GetEncoder();
+  frc::DifferentialDrive* m_robotDrive = nullptr;
+
+};
