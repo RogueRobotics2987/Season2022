8dc40d436a5918c8aff136886d14007e8c5490cd
=davidorser <=davidorser@gmail.com>
2022-01-30 11:41:01 -0600

    Fixed minor details of library includes

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -6,11 +6,13 @@
 
 #include <frc/TimedRobot.h>
 #include <iostream>  // Only need if you use cout (don't)
-#include <units/time.h> // Only include the units you actually need/use.
-                        // all "units" libraries automatically include all fmt comamnds
+//#include <units/time.h>   // all "units" libraries automatically include all fmt comamnds
+                            // currently all robotics (timed and command-based) include units automatically.
 
-#include <fmt/format.h> // Technically the required library for fmt::format
-#include <fmt/printf.h> // Technically the required library for fmt::print
+//  You may need one of the below items if you are running CLI C++, it is good practice to include one.
+//#include <fmt/format.h> // Technically the required library for fmt::format
+//#include <fmt/printf.h> // Technically the required library for fmt::print
+//#indclude <fmt/core.h>  // Techinically the required library for all fmt functions.
 
 class Robot : public frc::TimedRobot {
  public:
