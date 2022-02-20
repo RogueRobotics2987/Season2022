23c0accd6d3bba9b00cc42c358ee813907944458
= <=>
2022-02-20 16:56:48 -0600

    changed button

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -15,17 +15,17 @@ RobotContainer::RobotContainer() : m_autonomousCommand(drivetrain,2.0,-4.0){
 
 void RobotContainer::ConfigureButtonBindings() {
   // Configure your button bindings here
-  frc2::JoystickButton(&xbox, 4).WhenPressed(&m_intakeIn); 
-  frc2::JoystickButton(&xbox, 4).WhenReleased(&m_intakeInRelease); 
+  frc2::JoystickButton(&stick2, 1).WhenPressed(&m_intakeIn); //wwas xbox 4
+  frc2::JoystickButton(&stick2, 1).WhenReleased(&m_intakeInRelease); 
 
-  frc2::JoystickButton(&xbox, 2).WhenPressed(&m_intakeOut); 
-  frc2::JoystickButton(&xbox, 2).WhenReleased(&m_intakeOutRelease); 
+  frc2::JoystickButton(&stick1, 1).WhenPressed(&m_intakeOut); //was xbox 2
+  frc2::JoystickButton(&stick1, 1).WhenReleased(&m_intakeOutRelease); 
 
   frc2::JoystickButton(&xbox, 3).WhenPressed(&m_conveyerForward); //was xbox 1
   frc2::JoystickButton(&xbox, 3).WhenReleased(&m_conveyerForwardRelease); //was xbox 1
 
-  frc2::JoystickButton(&stick2, 2).WhenPressed(&m_conveyerBackward); 
-  frc2::JoystickButton(&stick2, 2).WhenReleased(&m_conveyerBackwardRelease); 
+  frc2::JoystickButton(&xbox, 1).WhenPressed(&m_conveyerBackward); //was stick2 2
+  frc2::JoystickButton(&xbox, 1).WhenReleased(&m_conveyerBackwardRelease); 
 
   // frc2::JoystickButton(&xbox, 3).WhenPressed(&m_shooter2000); 
   // frc2::JoystickButton(&xbox, 3).WhenReleased(&m_shooterStop); 
