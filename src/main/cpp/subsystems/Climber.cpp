afd30a9c1bc56cb04c4d61f08619f1a0d5456cf0
=reiols01 <=soccerlife7316@gmail.com>
2022-03-02 12:45:24 -0600

    attempt at climber error limit

--- src/main/cpp/subsystems/Climber.cpp
+++ src/main/cpp/subsystems/Climber.cpp
@@ -5,15 +5,25 @@
 #include "subsystems/Climber.h"
 
 Climber::Climber(){
-    m_climbMotorLeft.Follow(m_climbMotorRight, true);
+    //m_climbMotorLeft.Follow(m_climbMotorRight, true);
+    frc::SmartDashboard::PutNumber("climbkValue", climbKValue);
+
 }
 
 // This method will be called once per scheduler run
 void Climber::Periodic() {
     // climbVal = 0.0;
     frc::SmartDashboard::PutNumber("climbVal", climbVal);
+    frc::SmartDashboard::GetNumber("climbKValue", climbKValue);
 
-    m_climbMotorRight.Set(climbVal);
+    error = re_climbMotorRight.GetPosition() - re_climbMotorLeft.GetPosition();
+    if (error > 0.2) {
+        error = 0.2;
+    } else if (error < -0.2){
+        error = -0.2;
+    }
+    m_climbMotorRight.Set(climbVal + error*climbKValue);
+    m_climbMotorLeft.Set(climbVal - error*climbKValue);
     frc::SmartDashboard::PutNumber("RightClimb", RC); 
     frc::SmartDashboard::PutNumber("LeftClimb", LC); 
     m_climbMotorLeft.GetOutputCurrent();
@@ -37,9 +47,9 @@ void Climber::ClimbFunction(double climbUpVal, double climbDownVal){
     frc::SmartDashboard::PutNumber("climbDownVal", climbDownVal);
 
     if((fabs(climbUpVal) > upDeadzone) && (climbDownVal < downDeadzone)){
-        climbVal = climbUpVal - upDeadzone;
+        climbVal = (climbUpVal - upDeadzone)*0.5;
     } else if((fabs(climbDownVal) > downDeadzone) && (climbUpVal < upDeadzone)){
-        climbVal = -(climbDownVal - downDeadzone);
+        climbVal = -(climbDownVal - downDeadzone)*0.5;
     } else {
         climbVal = 0;
     }
