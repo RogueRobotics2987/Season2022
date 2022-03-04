7c169bf2ee2d4eafcc8a17210ace8a57747ee38f
=RyanLe2005 <=Ryanlebiz@gmail.com>
2022-03-04 11:44:32 -0600

    shooter preshoot speed at max

--- src/main/cpp/subsystems/TurretSubsystem.cpp
+++ src/main/cpp/subsystems/TurretSubsystem.cpp
@@ -65,8 +65,8 @@ void TurretSubsystem::Periodic() {
     } else if (TurretState == PRESHOOT_RAISE){
         
         if(re_vTurretMotorRight.GetPosition() > -400) {
-            m_vTurretMotorRight.Set(-0.2);
-            m_vTurretMotorLeft.Set(-0.2);
+            m_vTurretMotorRight.Set(-1.0);
+            m_vTurretMotorLeft.Set(-1.0);
         } else {
             m_vTurretMotorRight.Set(0.0);
             m_vTurretMotorLeft.Set(0.0);
