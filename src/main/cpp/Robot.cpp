744a55a78730ca7c5d2d5416651d64e5b99848ed
=reiols01 <=soccerlife7316@gmail.com>
2022-03-05 09:37:09 -0600

    Set close as default

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -10,9 +10,9 @@
 
 void Robot::RobotInit() {
   m_chooser.AddOption("None", nullptr);
-  m_chooser.AddOption("Close Ball Auto", m_container.GetCloseBallAuto());
+  m_chooser.SetDefaultOption("Close Ball Auto", m_container.GetCloseBallAuto());
   m_chooser.AddOption("Three Ball Auto", m_container.GetThreeBallAuto());
-  m_chooser.SetDefaultOption("Two Ball Auto", m_container.GetTwoBallAuto());
+  m_chooser.AddOption("Two Ball Auto", m_container.GetTwoBallAuto());
   frc::SmartDashboard::PutData(&m_chooser);
 
   // automatically start camera server
