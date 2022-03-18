f2a0184bb15bc305960c35e65c05b85d900f7553
calvinlemke <calvinblemke@gmail.com>
2022-03-17 19:52:57 -0500

    Fixed StartGame Spelling Error

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -168,7 +168,7 @@ frc2::Command* RobotContainer::GetCloseBallAuto() {
   config.SetKinematics(DriveConstants::kDriveKinematics);
   // config.AddConstraint(autoVoltageConstraint);
 
-  std::string startGameFile = frc::filesystem::GetDeployDirectory() + "/paths/output/startGame.wpilib.json";
+  std::string startGameFile = frc::filesystem::GetDeployDirectory() + "/paths/output/StartGame.wpilib.json";
     // wpi::sys::path::append(startGameFile, "paths/startGame.wpilib.json");
     frc::Trajectory startGame = frc::TrajectoryUtil::FromPathweaverJson(startGameFile);
 
