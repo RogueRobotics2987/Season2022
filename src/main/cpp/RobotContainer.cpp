995c1e5b049c2a55576590037798854c0429f547
=reiols01 <=soccerlife7316@gmail.com>
2022-03-19 13:20:24 -0500

    auto: switched conveyor to be on longer

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -123,17 +123,17 @@ frc2::Command* RobotContainer::GetTwoBallAuto() {
         //frc2::InstantCommand([this] {m_turret.setManuelAimOn();}, {&m_turret}),
         frc2::InstantCommand([this] {intake.IntakeInRelease();}, {&intake}),
         frc2::ParallelCommandGroup(
-          TimerCMD(0.5),
+          TimerCMD(1.0),
           frc2::InstantCommand([this] {intake.ConveyorForward();}, {&intake})
         ),
         frc2::InstantCommand([this] {intake.ConveyorForwardRelease();}, {&intake}),
         TimerCMD(0.5),
         
-        frc2::ParallelCommandGroup(
+        /*frc2::ParallelCommandGroup(
           TimerCMD(0.5),
           frc2::InstantCommand([this] {intake.ConveyorForward();}, {&intake})
         ),
-        frc2::InstantCommand([this] {intake.ConveyorForwardRelease();}, {&intake}),
+        frc2::InstantCommand([this] {intake.ConveyorForwardRelease();}, {&intake}),*/
         std::move(ramseteCommandTwoBall1_3),
        /* frc2::ParallelCommandGroup(
           frc2::InstantCommand([this] {m_turret.setAutoAimOn();}, {&m_turret}),
