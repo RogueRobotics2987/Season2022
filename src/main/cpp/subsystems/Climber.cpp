23b07782aae078b50c25f0e33a290ed47def4118
= <=>
2022-03-22 16:27:35 -0500

    deleted comments on lines 80 and 81 in climber.cpp

--- src/main/cpp/subsystems/Climber.cpp
+++ src/main/cpp/subsystems/Climber.cpp
@@ -77,10 +77,10 @@ void Climber::ClimbFunction(double climbUpVal, double climbDownVal){
     // climbVal = 0.0
 }
 void Climber::ClimbServoLock(){
-    m_climbServoRight.SetAngle(120); //don't know on angles
+    m_climbServoRight.SetAngle(120);
     m_climbServoLeft.SetAngle(120);
 }
 void Climber::ClimbServoUnlock(){
-    m_climbServoRight.SetAngle(0); //don't know on angles
+    m_climbServoRight.SetAngle(0);
     m_climbServoLeft.SetAngle(0);
 }
