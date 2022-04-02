8f84c0254c94d194a383c26d778b4e7bf5524d8e
= <=>
2022-04-02 13:06:30 -0500

    changed drive motor 4 to 13

--- src/main/include/subsystems/DriveTrain.h
+++ src/main/include/subsystems/DriveTrain.h
@@ -49,7 +49,7 @@ class DriveTrain : public frc2::SubsystemBase {
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
   rev::CANSparkMax LeftBack = rev::CANSparkMax(3, rev::CANSparkMax::MotorType::kBrushless);//56 on Howie
-  rev::CANSparkMax LeftFront = rev::CANSparkMax(4, rev::CANSparkMax::MotorType::kBrushless);//49 on Howie
+  rev::CANSparkMax LeftFront = rev::CANSparkMax(13, rev::CANSparkMax::MotorType::kBrushless);//49 on Howie
   rev::CANSparkMax RightBack = rev::CANSparkMax(16, rev::CANSparkMax::MotorType::kBrushless);//50 on Howie
   rev::CANSparkMax RightFront = rev::CANSparkMax(1, rev::CANSparkMax::MotorType::kBrushless);//46 on Howie
   frc::DifferentialDrive m_robotDrive{LeftFront, RightFront};
